38298fcfa61d709dd433962de92ba2c5802150d1
leandrocarra <leandrocarra@gmail.com>
2020-12-03 19:37:46 -0300

    api

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ const teste = () => {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet" />
       </Head>
-      {/* <button onClick={teste}>reer</button> */}
+      <button onClick={teste}>reer</button>
       <Register />
     </>
   )
