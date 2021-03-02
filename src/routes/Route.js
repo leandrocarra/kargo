d5f811bdd05b0a4c2b6ed9ed9145a4b9d1949f32
Alfinete <leandrocarra@gmail.com>
2021-03-02 16:50:25 -0300

    deploy

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -4,14 +4,12 @@ import { Route, Redirect } from 'react-router-dom';
 import LayoutDefault  from '~/pages/_layouts/Default';
 import LoginLayout from '~/pages/_layouts/SingIn';
 
-import {store} from '~/store';
-
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  // const { signed } = store.getState().auth;
+
   const signed  = true;
 
   if (!signed && isPrivate) {
