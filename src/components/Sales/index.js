42c0b76c98cdee0936498b82a049bbab1d1392fd
leandrocarra <leandro.carra-ext@ab-inbev.com>
2022-09-24 14:30:27 -0300

    add table
    '

--- src/components/Sales/index.js
+++ src/components/Sales/index.js
@@ -1,12 +1,80 @@
 import React from 'react';
 
+import RowList from '~/components/RowList'
 import BoxContent from '~/components/BoxContent';
+import {
+  SalesListTitles
+} from './styles';
+
+const xuxu = 
+  {
+    protocolo: '1231232112312',
+    comprador: 'nome fuladno',
+    transaction:{
+      data: '29/09/2022', 
+      time: '13:00',
+    },
+    itens : [
+      '21312312312312121212',
+    ],
+    originalValor: 'R$ 1.234,00',
+    originalPayed: 'R$ 1.234,00',
+    parcelas: '2x',
+    captured:{
+      data: '29/09/2022', 
+      time: '13:00',
+    },
+    status: 'APROVADO'
+  }
+
 
 function Sales() {
   return (
     <>
       <BoxContent>
-        VENDAS
+        <SalesListTitles>
+          <label>Protocolo</label>
+          <label>Comprador</label>
+          <label>Data Transição</label>
+          <label>Itens</label>
+          <label>Valor Original</label>
+          <label>Valor Pago</label>
+          <label>Parcelas</label>
+          <label>Data Captura</label>
+          <label>Status</label>
+        </SalesListTitles>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
+        <RowList params={xuxu}/>
       </BoxContent>
     </>
   )
---
+++ src/components/Sales/styles/index.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import {
+  gray_area,
+  gray_markup,
+  gray_line,
+  white
+} from '../../../styles/Colors';
+
+export const SalesListTitles = styled.div`
+  overflow: hidden;
+  label {
+    float: left;
+    width: 11.1%;
+    font-weight: 500;
+    justify-content: center;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    height: 50px;
+  }
+  border-bottom: 1px solid;
+  padding-bottom: 10px;
+  margin-bottom: 30px;
+`;
