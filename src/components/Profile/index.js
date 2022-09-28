009f452b0d961d21cf4b6c4e5c44251d3da4e230
leandrocarra <leandro.carra-ext@ab-inbev.com>
2022-09-28 17:41:16 -0300

    forms

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,31 +1,92 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Form, Input } from '@rocketseat/unform';
+import { Formik, Field, Form } from 'formik';
 
 import BoxContent from '~/components/BoxContent';
 
-import { Container } from './styles';
+import {
+  Container
+} from './styles';
 
-function Profile() {
-  const profile = useSelector(state => state.user.profile)
 
-  function handleSubmit(data) {
-    console.tron.log(data);
-  }
+function Profile() {  
 
   return (
     <BoxContent title='Profile'>
       <Container>
-        <Form initialData={profile} onSubmit={handleSubmit}>
-          <Input name="name" placeholder="Nome completo" />
-          <Input name="email"  type="email" placeholder="email" />
-          <hr />
-          <Input type="password" name="oldPassword" placeholder="Sua senha atual" />
-          <Input type="password" name="password" placeholder="Nova senha" />
-          <Input type="password" name="confirmPassword" placeholder="Confirmação de senha" />
-          <button type="submit">Atualizar Perfil</button>
-        </Form>
-        <button type="button">Sair da kargo Painel</button>
+        <h1>Sign Up</h1>
+        <Formik
+          initialValues={{
+            name: '',
+            email: '',
+            whatsapp: '',
+            fixedPhone: '',
+            celPhone: '',
+            password: '',
+            address: '',
+            cep: '',
+            logradouro: '',
+            number: '',
+            complement: '',
+            neighborhood: '',
+            city: '',
+            uf: ''
+          }}
+          onSubmit={async (values) => {
+            await new Promise((r) => setTimeout(r, 500));
+            alert(JSON.stringify(values, null, 2));
+          }}
+        >
+          <Form>
+            <label htmlFor="name">First Name</label>
+            <Field id="name" name="name" placeholder="Jane" />
+
+            <label htmlFor="email">Email</label>
+            <Field
+              id="email"
+              name="email"
+              placeholder="jane@acme.com"
+              type="email"
+            />
+
+            <label htmlFor="whatsapp">whatsapp</label>
+            <Field id="whatsapp" name="whatsapp" placeholder="Jane" />
+
+            <label htmlFor="fixedPhone">fixedPhone</label>
+            <Field id="fixedPhone" name="fixedPhone" placeholder="Jane" />
+
+            <label htmlFor="celPhone">celPhone</label>
+            <Field id="celPhone" name="celPhone" placeholder="Jane" />
+
+            <label htmlFor="password">password</label>
+            <Field id="password" name="password" placeholder="Jane" />
+
+            <label htmlFor="address">address</label>
+            <Field id="address" name="address" placeholder="Jane" />
+
+            <label htmlFor="cep">cep</label>
+            <Field id="cep" name="cep" placeholder="Jane" />
+
+            <label htmlFor="logradouro">logradouro</label>
+            <Field id="logradouro" name="logradouro" placeholder="Jane" />
+
+            <label htmlFor="number">number</label>
+            <Field id="number" name="number" placeholder="Jane" />
+
+            <label htmlFor="complement">complement</label>
+            <Field id="complement" name="complement" placeholder="Jane" />
+
+            <label htmlFor="neighborhood">neighborhood</label>
+            <Field id="neighborhood" name="neighborhood" placeholder="Jane" />
+
+            <label htmlFor="city">city</label>
+            <Field id="city" name="city" placeholder="Jane" />
+
+            <label htmlFor="uf">uf</label>
+            <Field id="uf" name="uf" placeholder="Jane" />
+            
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
       </Container>
     </BoxContent>
   );
