a268c1424ac9551a9dbc81c95208bc6fdd543afa
leandrocarra <leandro.carra-ext@ab-inbev.com>
2022-11-08 11:59:45 -0300

    login

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://104.154.94.242:8000',
-  baseURL: 'https://104.154.94.242:8000',
+  baseURL: 'http://104.154.94.242:8000',
 });
 
 export default api;
